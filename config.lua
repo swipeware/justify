f43043c374009074576f38d6b6e2004dc938a664
Ingemar Bergmark <ingemar@swipeware.com>
2014-07-02 20:00:20 +0900

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,16 @@
+-- config.lua
+--
+-- Ingemar Bergmark
+-- www.swipeware.com
+
+application = {
+    content = {
+        width     = 320,
+        height    = 480,
+        scale     = "letterbox",
+        fps       = 60,
+        antialias = false,
+        xAlign    = "center",
+        yAlign    = "center"    
+    }
+}
